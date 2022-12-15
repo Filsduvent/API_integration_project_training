8ebd3f01ddf8332c20cfc7c6e4eb5dae75dafb4a
Filsduvent <dondalbrownirakoz@gmail.com>
2022-12-15 12:25:14 +0200

    Appbar widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:store_api_flutter_course/widget/appbar_icons.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,8 +16,13 @@ class _HomeScreenState extends State<HomeScreen> {
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      child: const Scaffold(
-        body: Center(child: Text("Welcome to this course")),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+          leading: AppBarIcons(function: () {}, icon: IconlyBold.category),
+          actions: [AppBarIcons(function: () {}, icon: IconlyBold.user3)],
+        ),
+        body: const Center(child: Text("Welcome to this course")),
       ),
     );
   }
