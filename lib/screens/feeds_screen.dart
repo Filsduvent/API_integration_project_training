6cc2b7f054175ccd76b944186e0ec67f1bafdbfa
Filsduvent <dondalbrownirakoz@gmail.com>
2022-12-16 11:06:36 +0200

    Home grid design, all product design,category,product details

---
+++ lib/screens/feeds_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../widgets/feeds_widget.dart';
+
+class FeedsScreen extends StatelessWidget {
+  const FeedsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('All Products'),
+        // leading: AppBarIcons(function: () {}, icon: IconlyLight.arrowLeft),
+      ),
+      body: GridView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: 3,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 0.0,
+              mainAxisSpacing: 0.0,
+              childAspectRatio: 0.7),
+          itemBuilder: (ctx, index) {
+            return const FeedsWidget();
+          }),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,15 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:store_api_flutter_course/consts/global_colors.dart';
+import 'package:store_api_flutter_course/screens/categories_screen.dart';
+import 'package:store_api_flutter_course/screens/users_screen.dart';
 import 'package:store_api_flutter_course/widgets/appbar_icons.dart';
 
+import '../widgets/feeds_widget.dart';
 import '../widgets/sale_widget.dart';
+import 'feeds_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -28,6 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
@@ -35,8 +42,26 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Home'),
-          leading: AppBarIcons(function: () {}, icon: IconlyBold.category),
-          actions: [AppBarIcons(function: () {}, icon: IconlyBold.user3)],
+          leading: AppBarIcons(
+              function: () {
+                Navigator.push(
+                    context,
+                    PageTransition(
+                        child: const CategoriesScreen(),
+                        type: PageTransitionType.fade));
+              },
+              icon: IconlyBold.category),
+          actions: [
+            AppBarIcons(
+                function: () {
+                  Navigator.push(
+                      context,
+                      PageTransition(
+                          child: const UsersScreen(),
+                          type: PageTransitionType.fade));
+                },
+                icon: IconlyBold.user3)
+          ],
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
@@ -68,9 +93,69 @@ class _HomeScreenState extends State<HomeScreen> {
                     )),
               ),
               const SizedBox(
-                height: 20.0,
+                height: 18,
               ),
-              const SaleWidget(),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: size.height * 0.25,
+                        child: Swiper(
+                          itemCount: 3,
+                          itemBuilder: (context, index) {
+                            return const SaleWidget();
+                          },
+                          autoplay: true,
+                          pagination: const SwiperPagination(
+                              alignment: Alignment.bottomCenter,
+                              builder: DotSwiperPaginationBuilder(
+                                  color: Colors.white,
+                                  activeColor: Colors.red)),
+                          // control: const SwiperControl(),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            const Text(
+                              'Latest Products',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            const Spacer(),
+                            AppBarIcons(
+                                function: () {
+                                  Navigator.push(
+                                      context,
+                                      PageTransition(
+                                          child: const FeedsScreen(),
+                                          type: PageTransitionType.fade));
+                                },
+                                icon: IconlyBold.arrowRight2)
+                          ],
+                        ),
+                      ),
+                      GridView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: 3,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  crossAxisSpacing: 0.0,
+                                  mainAxisSpacing: 0.0,
+                                  childAspectRatio: 0.7),
+                          itemBuilder: (ctx, index) {
+                            return const FeedsWidget();
+                          })
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
